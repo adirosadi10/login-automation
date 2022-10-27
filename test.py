cf50b3c20cd915fe322b5f15af118b205d5eeb85
Adi Rosadi <adirosadi49@gmail.com>
2022-10-27 22:06:35 +0700

    login user

--- test.py
+++ test.py
@@ -13,42 +13,24 @@ import time
 #options.add_experimental_option('excludeSwitches', ['enable-logging'])
 #driver = webdriver.Chrome(options=options)
 driver = webdriver.Chrome()
+driver.maximize_window()
 
 
 
 #URL
-driver.get("https://sandbox.rsintanhusada.com/index.php?r=farmasiApotek&modul_id=10")
+#driver.get("https://sandbox.rsintanhusada.com/index.php?r=farmasiApotek&modul_id=10")
+driver.get("https://dev.ihospital.id/index.php?r=site/login")
 
 #LOGIN
-driver.find_element(By.ID,'LoginForm_username').send_keys('sysadmin')
-driver.find_element(By.ID,'LoginForm_password').send_keys('Rsih-123')
-#se = Select(driver.findElement(By.xpath("//*[@id='oldSelectMenu']")));
-#se = Select(driver.find_element(By.ID,'LoginForm_instalasi')).click()
-driver.find_element(By.ID,'LoginForm_instalasi').click()
-time.sleep(5)
-#se.selectByIndex(3)
-#driver.find_element(By.CSS_SELECTOR,"Option.[value='79']")
-#
-#pyautogui.countdown
+##username dan password
+driver.find_element(By.ID,'LoginForm_username').send_keys('rosadi')
+driver.find_element(By.ID,'LoginForm_password').send_keys('Rosadi-10')
+time.sleep(3)
+driver.find_element(By.ID,'login-button').click()
 
-
-
-
-#driver.implicitly_wait(10)
-#pilih = Select(driver.find_element(By.ID,'LoginForm_instalasi'))
-#pilih.select_by_visible_text('It/itkom/sirs/edp')
-#pilih.select_by_value("79")
-#select_by_value
-#(79)
-#driver.find_element(By.ID,'LoginForm_instalasi').click()
-#Select instalasi = new Select(driver.find_element(By.ID('LoginForm_instalasi')))
-#driver.find_element(By.CSS_SELECTOR,'option.value').click()
-#send_keys('sysadmin')
-#driver.find_element(By.ID,'LoginForm_ruangan').send_keys('Rsih-123')
-#driver.find_element(By.ID,'LoginForm_modul').send_keys('sysadmin')
-#driver.find_element(By.ID,'login-button').click()
-
-
-
-
+#MASUK KE MENU LOGIN PEMAKAI
+#driver.find_element(By.LINK_TEXT,value='/index.php?r=sistemAdministrator/LoginpemakaiK/Admin&modulId=1')
+#driver.find_element(By.XPATH, '/html/body/div[1]/header/div[1]/ul[1]/li[6]/ul/li[2]/a').click()
+#driver.find_element(By.CSS_SELECTOR, value='.navbar-nav > li:nth-child(6) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)').click()
+driver.find_element(By.CLASS_NAME,'icon-batalkeluarumum').click()
 
